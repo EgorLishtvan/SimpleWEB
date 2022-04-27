6993f02ea4ee420022ab1e638e6a94f55fdfd03d
EgorLishtvan <87782615+EgorLishtvan@users.noreply.github.com>
2022-04-27 13:54:49 +0300

    Add files via upload

---
+++ 1/secreet.js
@@ -0,0 +1,9 @@
+﻿function red(){
+	alert('Вы выбрали красный квадрат!');
+}
+function yellow(){
+	alert('Вы выбрали желтый квадрат!');
+}
+function green(){
+	alert('Вы выбрали зеленый квадрат!');
+}
